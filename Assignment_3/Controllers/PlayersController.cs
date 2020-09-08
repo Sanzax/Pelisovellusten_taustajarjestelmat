34feedcc8cfde26b7725ceb3e1a25862150a03c5
Santeri Mäkinen <santeri98makinen@gmail.com>
2020-09-08 15:14:34 +0300

    Assignment_3

---
+++ Assignment_3/Controllers/PlayersController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Assignment_3
+{
+    [ApiController]
+    [Route("/players")]
+    public class PlayersController : ControllerBase
+    {
+        private readonly ILogger<PlayersController> _logger;
+        private readonly IRepository _repository;
+
+        public PlayersController(ILogger<PlayersController> logger, IRepository r)
+        {
+            _logger = logger;
+            _repository = r;
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<Player> Get(Guid id)
+        {
+            return await _repository.Get(id);
+        }
+        [HttpGet]
+        [Route("GetAll")]
+        public async Task<Player[]> GetAll()
+        {
+            return await _repository.GetAll();
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<Player> Create(NewPlayer player)
+        {
+            _logger.LogInformation("Creating player with name " + player.Name);
+            Player p = new Player()
+            {
+                Name = player.Name,
+                Id = Guid.NewGuid()
+            };
+
+            p.CreationTime = DateTime.Now;
+
+            await _repository.Create(p);
+
+            return p;
+        }
+
+        [HttpPost]
+        [Route("Modify")]
+        public async Task<Player> Modify(Guid id, ModifiedPlayer player)
+        {
+            return await _repository.Modify(id, player);
+        }
+
+        [HttpPost]
+        [Route("Delete")]
+        public async Task<Player> Delete(Guid id)
+        {
+            return await _repository.Delete(id);
+        }
+    }
+}
---
+++ Assignment_3/FileRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace Assignment_3
+{
+    public class FileRepository : IRepository
+    {
+        public Task<Player> Get(Guid id)
+        {
+            Task<Player[]> players = GetAll();
+
+            foreach (Player p in players.Result)
+            {
+                if (p.Id == id)
+                    return Task.FromResult(p);
+            }
+            return null;
+        }
+        public Task<Player[]> GetAll()
+        {
+            Player[] players;
+
+            string text = File.ReadAllText("game-dev.txt");
+            if(text == "")
+            {
+                return Task.FromResult(Array.Empty<Player>());
+            }
+
+            players = JsonConvert.DeserializeObject<Player[]>(text);
+
+            return Task.FromResult(players);
+        }
+        public Task<Player> Create(Player player)
+        {
+            List<Player> playerList = GetAll().Result.ToList<Player>();
+
+            playerList.Add(player);
+
+            CreateJson(playerList.ToArray(), "game-dev.txt");
+
+            return Task.FromResult(player);
+        }
+        public Task<Player> Modify(Guid id, ModifiedPlayer player)
+        {
+            Player[] players = GetAll().Result;
+
+            foreach(Player p in players)
+            {
+                if(p.Id == id)
+                {
+                    p.Score = player.Score;
+                    CreateJson(players, "game-dev.txt");
+                    return Task.FromResult(p);
+                }
+            }
+            return null;
+        }
+
+        public Task<Player> Delete(Guid id)
+        {
+            List<Player> playerList = GetAll().Result.ToList<Player>();
+
+            foreach (Player p in playerList)
+            {
+                if (p.Id == id)
+                {
+                    playerList.Remove(p);
+                    CreateJson(playerList.ToArray(), "game-dev.txt");
+                    return Task.FromResult(p);
+                }
+            }
+            return null;
+        }
+
+        private void CreateJson(Player[] players, string path)
+        {
+            string jsonString = JsonConvert.SerializeObject(players);
+            File.WriteAllText(path, jsonString);
+        }
+    }
+}
